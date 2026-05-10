e74dc9ffbfde9cc6db189a9f6ece3d7f2ce5bb25
Athiyyah NH <athiyyah.nh9@gmail.com>
2026-05-10 21:08:21 +0700

    change list to dict (done)

--- market.py
+++ market.py
@@ -7,14 +7,8 @@ fruit = [
     {"stock": 10, "product": "Jeruk", "price": 15_000},
     {"stock": 10, "product": "Anggur", "price": 20_000},
 ]
-
-print(fruit)
-
-running =True
-while running :
-    # Main Menu
+def view_menu():# Main menu
     print("\n=== Selamat Datang di Pasar Buah ===")
-
     print("\nList Menu: ")
     print("1. Menampilkan daftar Buah")
     print("2. Menambah Buah")
@@ -22,121 +16,123 @@ while running :
     print("4. Membeli Buah")
     print("5. Exit Program")
     choice = input("Masukkan angka menu yang ingin dijalankan: ")
+    return choice
+
+def get_product():
+    print("\nDaftar Buah")
+    print(f"{"Index":<7}|{"Nama":<10}|{"Stock":<5}|{"Harga":<16}")
+    for idx in range(len(fruit)):
+        print (f"{idx:<7}|{fruit[idx]["product"]:<10}|{fruit[idx]["stock"]:<5}|{fruit[idx]["price"]:<16}")
+
+def add_product(product_name, product_stock = 0,product_price = 0) : # parameter yg mempunyai default ditaruh diakhir
+    product = {
+        "product" : product_name,
+        "stock" : product_stock,
+        "price" : product_price
+    }
+    print(product)
+    confirm = input("yakin untuk menambah produk (y/n)?")
+    if confirm.lower() == "y":
+        fruit.append(product)
+        print("Produk berhasil ditambahkan!")
+    else:
+        print("Produk batal ditambahkan")
     
+    get_product()
+    
+    
+def delete_product():
+    get_product()
+    delete_fruit= int(input("Masukkan index buah yang ingin dihapus: "))
+    if 0 <= delete_fruit < len(fruit):
+        deleted = fruit.pop(delete_fruit)
+        print(f"'{deleted['product']}' berhasil dihapus!")
+        get_product()
+    else: 
+        print("index tidak valid!")
+    
+    
+def add_to_cart(cart):
+    get_product()
+    buy_fruit = int(input("Masukkan index buah yang ingin dibeli : "))
+    buy_qty = int(input("Masukkan jumlah yang ingin dibeli    : "))
+    if not (0 <= buy_fruit < len(fruit)):
+        print("Index tidak valid!")
+        return
+    # Cek stock
+    if buy_qty > fruit[buy_fruit]["stock"]:
+        print(f"Stock tidak cukup, stock {fruit[buy_fruit]['product']} tinggal {fruit[buy_fruit]['stock']}")
+        return
+
+    # Masuk ke cart
+    cart.append({
+        "product": fruit[buy_fruit]["product"],
+        "qty":     buy_qty,
+        "price":   fruit[buy_fruit]["price"]
+    })
+    show_cart(cart)
+    
+def show_cart(cart):
+    print("\nIsi Cart")
+    print(f"{'Nama':<10}| {'Qty':<5}| {'Harga'}")
+    for item in cart:
+        print(f"{item['product']:<10}| {item['qty']:<5}| {item['price']}")
+
+
+def checkout(cart):
+    total_harga = 0
+    for item in cart:
+        total_harga += item["qty"] * item["price"]
+
+    # Tampilkan daftar belanja
+    print("\nDaftar Belanja")
+    print(f"{'Nama':<10}| {'Qty':<5}| {'Harga':<10}| {'Total Harga'}")
+    for item in cart:
+        print(f"{item['product']:<10}| {item['qty']:<5}| {item['price']:<10}| {item['qty'] * item['price']}")
+
+    print(f"Total Yang Harus Dibayar : {total_harga}")
+    
+    while True:
+        bayar = int(input("Masukkan jumlah uang: Rp"))
+        if bayar >= total_harga:
+            break
+        print(f"Uang kurang! Total: Rp{total_harga:,}, uang kamu: Rp{bayar:,}")
+
+    print("Terima kasih sudah berbelanja!")
+    print(f"Uang kembali: Rp{bayar - total_harga:,}")
+    
+def buy_product():
+    cart = []
+    while True:
+        add_to_cart(cart)
+        cont = input("\nMau beli yang lain? (y/n): ")
+        if cont.lower() != "y":
+            break
+    checkout(cart)
+
+
+
+
+running =True
+while running :
+    choice = view_menu()
     if choice == "1": #Menampilkan daftar buah
-        print("\nDaftar Buah")
-        print(f"{"Index":<7}|{"Nama":<10}|{"Stock":<5}|{"Harga":<16}")
-        for idx in range(len(fruit)):
-            print (f"{idx:<7}|{fruit[idx]["product"]:<10}|{fruit[idx]["stock"]:<5}|{fruit[idx]["price"]:<16}")
+        get_product()
         
     elif choice == "2": #Menambah buah
-        while True:
-            add_fruit = input("Masukkan nama buah: ")
-            add_stock = int(input("Masukkan stock buah: "))
-            add_price = int(input("Masukkan harga buah: "))
-            
-            plus_fruit = [{"stock": add_stock, "product": add_fruit, "price": add_price}]
-            fruit.append(plus_fruit)
-            
-            confirm_msg = input("Lanjut tambah buah (yes:jika lanjut)?: ")
-            if confirm_msg.lower() != "yes":
-                print("Tambah buah selesai")
-                
-            print(f"{"Index":<7}|{"Nama":<10}|{"Stock":<5}|{"Harga":<16}")
-            for idx in range(len(fruit)):
-                print (f"{idx:<7}|{fruit[idx]["product"]:<10}|{fruit[idx]["stock"]:<5}|{fruit[idx]["price"]:<16}")
-            break
+        print("\nTambah Daftar Buah")
+        product_name = input("Masukkan nama buah: ")
+        product_stock = int(input("Masukkan stok buah: "))
+        product_price = int(input("Masukkan harga buah: "))
+        add_product(product_name,product_stock,product_price)
         
     elif choice == "3": #Menghapus buah
-        print("\nDaftar Buah")
-        print(f"{"Index":<7}|{"Nama":<10}|{"Stock":<5}|{"Harga":<16}")
-        for idx in range(len(fruit)):
-            print (f"{idx:<7}|{fruit[idx]["product"]:<10}|{fruit[idx]["stock"]:<5}|{fruit[idx]["price"]:<16}")
-        delete_fruit= int(input("Masukkan index buah yang ingin dihapus: "))
-        fruit.pop(delete_fruit)
-        
-        print(f"{"index":<6}{"Nama":<10}{"Stock":<12}{"Harga"}")
-        for idx in range(len(fruit)):
-            print (f"{idx:<7}|{fruit[idx]["product"]:<10}|{fruit[idx]["stock"]:<5}|{fruit[idx]["price"]:<16}")
+       delete_product()
             
     elif choice == "4": # membeli buah
-        cart = []
-        while True:
-            print("\nDaftar Buah")
-            print(f"{'Index':<7}| {'Nama':<10}| {'Stock':<7}| {'Harga'}")
-            for idx in range(len(fruit)):
-                print(f"{idx:<7}| {fruit[idx]['product']:<10}| {fruit[idx]['stock']:<7}| {fruit[idx]['price']}")
-
-            buy_fruit = int(input("Masukkan index buah yang ingin dibeli : "))
-
-            buy_qty = int(input("Masukkan jumlah yang ingin dibeli    : "))
-
-            # Cek stock
-            if buy_qty > fruit[buy_fruit]["stock"]:
-                print(f"Stock tidak cukup, stock {fruit[buy_fruit]['product']} tinggal {fruit[buy_fruit]['stock']}")
-                continue
-
-            # Masuk ke cart
-            cart.append({
-                "product": fruit[buy_fruit]["product"],
-                "qty":     buy_qty,
-                "price":   fruit[buy_fruit]["price"]
-            })
-
-            # Tampilkan isi cart
-            print("\nIsi Cart")
-            print(f"{'Nama':<10}| {'Qty':<5}| {'Harga'}")
-            for item in cart:
-                print(f"{item['product']:<10}| {item['qty']:<5}| {item['price']}")
-
-            cont = input("Mau beli yang lain? (ya/tidak) : ")
-            if cont.lower() != "ya":
-                break
-            
-
-        # Hitung total belanja
-        total_harga = 0
-        for item in cart:
-            total_harga += item["qty"] * item["price"]
-
-        # Tampilkan daftar belanja
-        print("\nDaftar Belanja")
-        print(f"{'Nama':<10}| {'Qty':<5}| {'Harga':<10}| {'Total Harga'}")
-        for item in cart:
-            print(f"{item['product']:<10}| {item['qty']:<5}| {item['price']:<10}| {item['qty'] * item['price']}")
-
-        print(f"Total Yang Harus Dibayar : {total_harga}")
-
-        # Input uang,minta ulang kalau kurang dan tampilkan kembalian
-        while True:
-            bayar = int(input("Masukkan jumlah uang : "))
-            if bayar >= total_harga:
-                break
-            print(f"Uang kurang! Total belanja {total_harga}, uang kamu {bayar}")
-
-        print("Terima kasih")
-        print(f"Uang kembali anda : {bayar - total_harga}")
-
-        # Kurangi stock buah yang dibeli
-        for item in cart:
-            for buah in fruit:
-                if buah["product"] == item["product"]:
-                    buah["stock"] -= item["qty"]
-                    
+        buy_product()
+  
     elif choice == "5":
-        break
-        
-
-        
-     
-            
-    
-        
-    
-        
-        
-        
-        
-
-
-
+        running = False
+    else:
+        print("\nPilihan tidak ada di dalam menu\n")    
